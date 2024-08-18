b6114600174dc864c8891ee9eb4ed63af637f57a
eskainah <eskainah@gmail.com>
2024-08-18 20:51:20 +0300

    create a book list view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from relationship_app.models import Book
 # Create your views here.
+
+def bookListView(request):
+    books = Book.objects.all()
+    context = {'book_List': books}
+    return render(request, 'relationship_app/list_books.html', context)
