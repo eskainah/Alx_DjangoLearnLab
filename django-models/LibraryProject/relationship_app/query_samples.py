de1f4de7074b7e19eff468f2f65247319293d491
eskainah <eskainah@gmail.com>
2024-08-18 20:08:26 +0300

    list books

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,6 +5,12 @@ def query_books_by_author(name):
     books = Book.objects.filter(author=author)
     return books
 
+def list_books_in_library(library_name):
+    
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+
 def retrieve_librarian(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
