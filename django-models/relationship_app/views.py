0fbe00e8c6ea89cf205763bcd6f8038cc950408a
eskainah <eskainah@gmail.com>
2024-08-18 21:14:50 +0300

    variable name update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,7 +10,7 @@ def list_books(request):
     context = {'book_List': books}
     return render(request, 'relationship_app/list_books.html', context)
 
-class LibraryDetailsView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
