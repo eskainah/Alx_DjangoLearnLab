a152b1d5ef08440b2d7de9354de6026f8b8863b7
eskainah <eskainah@gmail.com>
2024-08-18 20:54:50 +0300

    create a view using class

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from relationship_app.models import Book
+from relationship_app.models import Book, Library, Author, Librarian
+from django.views.generic import DetailView
 # Create your views here.
 
 def bookListView(request):
     books = Book.objects.all()
     context = {'book_List': books}
     return render(request, 'relationship_app/list_books.html', context)
+
+class Liberary_DetailsView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+    
