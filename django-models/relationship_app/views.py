0caed0800a5b9f9a23c21fda4abf8be67fa4fbde
eskainah <eskainah@gmail.com>
2024-08-18 21:36:58 +0300

    user authencation

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 # Create your views here.
 
 def list_books(request):
@@ -18,3 +28,51 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         book = self.get_object()
         context['average_rating'] = book.get_average_rating() 
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/register.html"
+
+def LogoutView(request):
+    return render(request, 'logout.html')
+
+def LoginView(request):
+    return render(request, 'login.html')
+
+def Register(request):
+    return render(request, 'register.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'authentication/login.html'
+    redirect_authenticated_user = True
+
+# Logout view
+class CustomLogoutView(LogoutView):
+    template_name = 'authentication/logout.html'
+
+# Registration view
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'authentication/register.html'
+    success_url = reverse_lazy('login')
+
+"UserCreationForm()", "relationship_app/register.html"
+
+def check_role(user, role):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == role
+
+# Admin view
+@user_passes_test(lambda user: check_role(user, 'Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(lambda user: check_role(user, 'Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(lambda user: check_role(user, 'Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
