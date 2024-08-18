ee709cae597e63a514f984221b1a0c441fd34e3e
eskainah <eskainah@gmail.com>
2024-08-18 18:55:14 +0300

    create a new directory ca;; django models

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    # Columns to display in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add filters for the author and publication year fields
+    list_filter = ('author', 'publication_year')
+    
+    # Add search functionality for the title and author fields
+    search_fields = ('title', 'author')
+
+# Register your models here.
+admin.site.register(Book)
