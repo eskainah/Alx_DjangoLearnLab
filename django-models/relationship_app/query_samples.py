5431ded959a9552d447c166707ff70a4e9cd6adf
eskainah <eskainah@gmail.com>
2024-08-18 20:19:42 +0300

    update author query

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-def query_books_by_author(name):
-    author = Author.objects.get(name=name)
+def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
     books = Book.objects.filter(author=author)
-    return author
+    return books
 
 def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
