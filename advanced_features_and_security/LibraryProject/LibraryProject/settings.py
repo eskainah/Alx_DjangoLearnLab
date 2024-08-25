2465e381e8f4ec67a8e3d597fabfc723b120f3d9
eskainah <eskainah@gmail.com>
2024-08-25 19:36:50 +0300

    update

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
 MIDDLEWARE = [
