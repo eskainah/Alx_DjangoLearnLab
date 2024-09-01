75e1f3c5c4923d2c929eb9cabb4499d1a6733088
eskainah <eskainah@gmail.com>
2024-09-01 21:32:08 +0300

    update views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
-from rest_framework import viewsets
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
 
