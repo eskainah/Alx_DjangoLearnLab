6d0fc4b071b81f80837f1ebf87d2bf0105c633a8
eskainah <eskainah@gmail.com>
2024-09-08 22:35:33 +0300

    set permission and authentication

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,30 +1,38 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
 
 class BookListView(generics.listAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookCreateView(generics.CreateAPIView):
-
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookUpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
