c494f8dacd09a5f0c6b668d9981b7c132142d417
eskainah <eskainah@gmail.com>
2024-09-08 22:57:59 +0300

    fix filter error

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
-
+from django_filters import rest_framework
 
 from .models import Book
 from .serializers import BookSerializer
