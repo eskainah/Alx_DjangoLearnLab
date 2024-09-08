6f03e5ce0c3f57038e6338c1e240dc111e3cab28
eskainah <eskainah@gmail.com>
2024-09-08 21:32:42 +0300

    create book and author serializers

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+#a serializers for book model 
+class BookSerializer(serializers.ModelSerializer)
+    class Meta:
+        model = Book
+        field = '__all__'
+    
+    def validate(self, data):
+        publication_year = data.get('publication_year')
+
+        if publication_year > date.today().year:
+            raise serializers.ValidationError(
+                {'publication_year': 'Pulication date cannot be in the future!'}
+            )
+        return data
+    
+#a serializer for author model 
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class meta:
+        model = Author
+        fields = ['author', 'books']
