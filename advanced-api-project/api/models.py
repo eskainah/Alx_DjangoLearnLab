4dd05d98e4202458f0d8686bb95e7f36aace164d
eskainah <eskainah@gmail.com>
2024-09-08 20:58:24 +0300

    create Author and Book models

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    author = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return self.author
+class Book(models.Model):
+    title = models.CharField(max_length= 100)
+    author = models.ForeignKey(
+        Author,
+        on_delete= models.CASCADE
+    )
+    publication_year = models.DateField()
+
+    def __str__(self):
+        return self.title
