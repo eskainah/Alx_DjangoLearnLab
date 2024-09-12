4feb57bfefcc7da3a8f01df6db32df1990e5cf6d
eskainah <eskainah@gmail.com>
2024-09-13 00:26:52 +0300

    add database setting

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -81,6 +81,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog',
+#         'USER': 'edkainah',
+#         'PASSWORD': 'user1@test',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
