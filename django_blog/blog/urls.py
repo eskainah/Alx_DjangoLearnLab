39a9e213a68b55b78ab4e66f8d9c217ca88d9089
eskainah <eskainah@gmail.com>
2024-09-15 20:16:58 +0300

    add search and tag url paths

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from django.contrib.auth import views as auth_views
 from .views import Register, Login, ProfileView, NewPost, Posts, PostDetail, PostEdit, PostDelete, \
                     CommentUpdateView, CommentCreateView, CommentDeleteView
@@ -18,4 +19,7 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name="comment_update"),
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name="create_comment"),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name="delete"),
+
+    path('search/', views.search_posts, name='search_posts'),
+    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),  # Filter posts by tag
 ]
