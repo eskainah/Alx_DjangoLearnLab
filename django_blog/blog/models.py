e4460777577d97891a0094ff558e41e2e3f3ceab
eskainah <eskainah@gmail.com>
2024-09-15 19:42:23 +0300

    create a tag model & add a many to many relation in post model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,6 +9,7 @@ class Post (models.Model):
     author = models.ForeignKey(User, 
                                on_delete=models.CASCADE, 
                                related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
     
     def __str__(self):
         return self.title
@@ -25,3 +26,9 @@ class Comment(models.Model):
     
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
