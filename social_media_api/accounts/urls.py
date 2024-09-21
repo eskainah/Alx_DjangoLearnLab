ac38696a4fcf64670bd7f83277f1c8cce4142b06
eskainah <eskainah@gmail.com>
2024-09-22 01:29:06 +0300

    create url patten in account app

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import RegisterView, LoginView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='user_registration'),
+    path('login/', LoginView.as_view(), name='login'),
+]
