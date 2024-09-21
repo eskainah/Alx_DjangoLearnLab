39ffed8c8a15a153f39bc761bb8e5615888d0f0f
eskainah <eskainah@gmail.com>
2024-09-22 02:25:37 +0300

    create post and comment view

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework import filters
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
