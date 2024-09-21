cce659baffc9a51ec50e80b05a1fdc371be47e0e
eskainah <eskainah@gmail.com>
2024-09-22 02:09:12 +0300

    make mirgration for posts app

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'posts',
     'accounts',
 ]
 AUTH_USER_MODEL = 'accounts.Customuser'
