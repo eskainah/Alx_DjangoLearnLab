ae75a5cc10f5821b39c33db8c0fa112b340b922f
eskainah <eskainah@gmail.com>
2024-09-22 02:08:17 +0300

    update views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework import generics, status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
