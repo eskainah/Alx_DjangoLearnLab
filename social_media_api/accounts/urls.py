0e45ff2336925f3d4a8b6e83d4d2831de2fd16d3
eskainah <eskainah@gmail.com>
2024-09-22 20:46:13 +0300

    set url for follow and unfollow views

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import RegisterView, LoginView, Follow_Unfollow_UserView
+from .views import RegisterView, LoginView, FollowUserView, UnfollowUserView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user_registration'),
     path('login/', LoginView.as_view(), name='login'),
-    path('follow-unfollow/<int:user_id>/', Follow_Unfollow_UserView.as_view(), name='follow_unfollow_user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
 ]
