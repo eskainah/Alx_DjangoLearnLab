8378f6c6ada7274ac37cc738ed5ae1472c762a87
eskainah <eskainah@gmail.com>
2024-09-22 20:38:03 +0300

    perform migration

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, Follow_Unfollow_UserView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user_registration'),
     path('login/', LoginView.as_view(), name='login'),
+    path('follow-unfollow/<int:user_id>/', Follow_Unfollow_UserView.as_view(), name='follow_unfollow_user'),
 ]
