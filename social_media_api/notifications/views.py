ea244386954a4458a6882e821b5138548a3f6121
eskainah <eskainah@gmail.com>
2024-09-22 21:49:49 +0300

    create notification view

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class NotificationListView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        notifications = request.user.notifications.filter(
+            read=False).order_by('-timestamp')
+        data = [{'actor': n.actor.username, 'verb': n.verb,
+                 'target': str(n.target), 'timestamp': n.timestamp} for n in notifications]
+        return Response(data)
