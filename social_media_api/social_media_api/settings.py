7eeb7fc53413ac5e09f974cacd069e1dc7496ee9
eskainah <eskainah@gmail.com>
2024-09-22 22:03:03 +0300

    update database settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #'PORT': config('DB_PORT', default='5432')
     }
 }
 
