fe75b6c358d9c3e180f84e3ae8485ed1670b4eb0
eskainah <eskainah@gmail.com>
2024-09-22 20:56:39 +0300

    create a like model in posts

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+
 # Create your models here.
+User = get_user_model()
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
@@ -10,8 +13,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -21,3 +22,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Ensure a user can like a post only once
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
