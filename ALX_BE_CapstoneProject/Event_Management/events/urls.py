883686045bba7fdf4912cbd30b29083167cbde60
eskainah <eskainah@gmail.com>
2024-09-30 22:07:55 +0300

    delete events app

--- ALX_BE_CapstoneProject/Event_Management/events/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register, login_view, home
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
-    path('', home, name='home'),
-]
