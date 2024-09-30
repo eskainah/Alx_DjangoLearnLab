39e258d1d9f6b81f5893dfe223951af0d72c82a6
eskainah <eskainah@gmail.com>
2024-09-30 20:35:44 +0300

    add app and restframework to settings

--- ALX_BE_CapstoneProject/Event_Management/Event_Management/settings.py
+++ ALX_BE_CapstoneProject/Event_Management/Event_Management/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Events',
 ]
 AUTH_USER_MODEL = 'Events.CustomUser'
 
