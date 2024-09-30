0c7b786c949e47bfc395f37ca4b8dc476f8fbb8e
eskainah <eskainah@gmail.com>
2024-09-30 22:07:17 +0300

    create a custom user model

--- ALX_BE_CapstoneProject/Event_Management/event/models.py
+++ ALX_BE_CapstoneProject/Event_Management/event/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser 
 from django.db import models
 
-# Create your models here.
+#create a Custom user model
+class CustomUser(AbstractUser):
+    middle_name = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(unique=True)
+
+    # Override the username field to ensure it's unique
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    def __str__(self):
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
