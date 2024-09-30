37706d606c0cb8502049ab9722f0c5e7e51d0313
eskainah <eskainah@gmail.com>
2024-09-30 20:51:28 +0300

    create a custom user form

---
+++ ALX_BE_CapstoneProject/Event_Management/Events/forms.py
@@ -0,0 +1,13 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    middle_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields + ('first_name', ' middle_name','last_name', 'email')
