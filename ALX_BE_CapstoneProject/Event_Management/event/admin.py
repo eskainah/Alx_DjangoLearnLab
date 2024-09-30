00d4294088d06982034e9921b0c443068dc8dd87
eskainah <eskainah@gmail.com>
2024-09-30 21:58:29 +0300

    register models in admin

--- ALX_BE_CapstoneProject/Event_Management/event/admin.py
+++ ALX_BE_CapstoneProject/Event_Management/event/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('middle_name',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('middle_name',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
