eb0e949aa6af3357e1535451e75ee558f0ffb266
eskainah <eskainah@gmail.com>
2024-10-02 02:51:08 +0300

    create an update_event view

--- ALX_BE_CapstoneProject/Event_Management/event/views.py
+++ ALX_BE_CapstoneProject/Event_Management/event/views.py
@@ -59,3 +59,15 @@ def event_list(request):
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
     return render(request, 'events/event_detail.html', {'event': event})
+
+def update_event(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Event updated successfully!')
+            return redirect('event_detail', pk=event.pk)
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'events/event_form.html', {'form': form})
