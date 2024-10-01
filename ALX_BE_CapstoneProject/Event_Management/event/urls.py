c6193335e65cba6cfbb7a6d407bcd7cb9c482cf7
eskainah <eskainah@gmail.com>
2024-10-02 02:58:44 +0300

    create urls for event CRUD operations

--- ALX_BE_CapstoneProject/Event_Management/event/urls.py
+++ ALX_BE_CapstoneProject/Event_Management/event/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import register, login_view, home
+from .views import register, login_view, home, event_list, create_event, event_detail, update_event, delete_event
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('', home, name='home'),
+    path('events/', event_list, name='event_list'),
+    path('events/create/', create_event, name='create_event'),
+    path('events/<int:pk>/', event_detail, name='event_detail'),
+    path('events/<int:pk>/edit/', update_event, name='update_event'),
+    path('events/<int:pk>/delete/', delete_event, name='delete_event'),
 ]
