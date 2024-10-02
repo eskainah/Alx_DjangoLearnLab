7f47c6a3789e192413acbcfd2091e146a4294cc2
eskainah <eskainah@gmail.com>
2024-10-02 05:17:10 +0300

    update url path

--- ALX_BE_CapstoneProject/Event_Management/event/urls.py
+++ ALX_BE_CapstoneProject/Event_Management/event/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (register, login_view, home, event_list, 
-                    create_event, event_detail, update_event, 
-                    delete_event, ProfileViewSet)
+from .views import (register, login_view, home, 
+                    EventViewSet, ProfileViewSet)
 
 router = DefaultRouter()
 router.register(r'profile', ProfileViewSet, basename='profile')
+router.register(r'events', EventViewSet)
 
 
 urlpatterns = [
@@ -13,9 +13,4 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('', home, name='home'),
-    path('event/', event_list, name='event_list'),
-    path('events/create/', create_event, name='create_event'),
-    path('event/<int:pk>/', event_detail, name='event_detail'),
-    path('event/<int:pk>/edit/', update_event, name='update_event'),
-    path('event/<int:pk>/delete/', delete_event, name='delete_event'),
 ]
