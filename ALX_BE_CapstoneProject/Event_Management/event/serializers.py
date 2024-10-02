544e64ea9761d2e216e0ec89b7006a6380fc30f3
eskainah <eskainah@gmail.com>
2024-10-02 04:49:56 +0300

    create event serializer

--- ALX_BE_CapstoneProject/Event_Management/event/serializers.py
+++ ALX_BE_CapstoneProject/Event_Management/event/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import (Profile,
+                     Event)
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['id', 'business_name']
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__' 
