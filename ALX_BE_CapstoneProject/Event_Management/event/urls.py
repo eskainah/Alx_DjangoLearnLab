5ec886925353c09a9d36ef88d7122417a4249ac3
eskainah <eskainah@gmail.com>
2024-10-02 04:16:38 +0300

    create url path for user profile

--- ALX_BE_CapstoneProject/Event_Management/event/urls.py
+++ ALX_BE_CapstoneProject/Event_Management/event/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import register, login_view, home, event_list, create_event, event_detail, update_event, delete_event
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (register, login_view, home, event_list, 
+                    create_event, event_detail, update_event, 
+                    delete_event, ProfileViewSet)
+
+router = DefaultRouter()
+router.register(r'profile', ProfileViewSet, basename='profile')
+
 
 urlpatterns = [
+     path('', include(router.urls)),
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('', home, name='home'),
