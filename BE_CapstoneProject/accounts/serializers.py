af4e26e89035040afff7c69c923092b70d3e36b1
eskainah <eskainah@gmail.com>
2024-10-04 23:01:57 +0300

    create CRUD operation on user account and ensure validation of data

--- BE_CapstoneProject/accounts/serializers.py
+++ BE_CapstoneProject/accounts/serializers.py
@@ -1,22 +1,40 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Profile, CustomUser
+from .models import Profile
+
+CustomUser = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=30)
-
+    first_name = serializers.CharField(max_length=30, required=True)
+    last_name = serializers.CharField(max_length=30, required=True)
     class Meta:
         model = CustomUser
-        fields = ('username', 'email',  'first_name', 'last_name', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'profile')
+        extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True}}
 
     def create(self, validated_data):
+        #Handle profile data when provided
+        profile_data = validated_data.pop('profile', None) 
+
         user = CustomUser(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
+        
+        # Create profile 
+        if profile_data:
+            Profile.objects.create(user=user, **profile_data)
         return user
     
+    #perform validation on username and email
+    def validate_username(self, value):
+        if CustomUser.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username is already taken.")
+        return value
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already registered.")
+        return value
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
