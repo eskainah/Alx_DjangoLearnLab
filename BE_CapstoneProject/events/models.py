9ff2c0cba72da17969f8653994963def524ddf27
eskainah <eskainah@gmail.com>
2024-10-06 02:52:48 +0300

    update model

--- BE_CapstoneProject/events/models.py
+++ BE_CapstoneProject/events/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db import models
-from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 class Event(models.Model):
