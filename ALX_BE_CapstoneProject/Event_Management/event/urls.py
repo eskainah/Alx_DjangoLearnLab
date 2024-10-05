5cb81d83efec74c3b03e6053b8c1a494754bfcc1
eskainah <eskainah@gmail.com>
2024-10-05 03:06:27 +0300

    delete files

--- ALX_BE_CapstoneProject/Event_Management/event/urls.py
+++ ALX_BE_CapstoneProject/Event_Management/event/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (CustomUserViewSet,
-                    EventViewSet, ProfileViewSet, AuthTokenViewSet)
+from .views import (EventViewSet)
 
 router = DefaultRouter()
-router.register(r'users', CustomUserViewSet, basename='user')
-router.register(r'profile', ProfileViewSet, basename='profile')
 router.register(r'events', EventViewSet)
-router.register(r'auth-token', AuthTokenViewSet, basename='auth-token')
 
 
 urlpatterns = [
