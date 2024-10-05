3605b4398ef65286b21c34f74f7ad441445c7757
eskainah <eskainah@gmail.com>
2024-10-06 02:53:47 +0300

    update views

--- BE_CapstoneProject/events/views.py
+++ BE_CapstoneProject/events/views.py
@@ -33,6 +33,7 @@ class EventViewSet(viewsets.ModelViewSet):
             return Response({'message': 'Event created', 'event': serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
     def update(self, request, pk=None):
         event = get_object_or_404(self.get_queryset(), pk=pk)
         serializer = self.get_serializer(event, data=request.data, partial=True)
