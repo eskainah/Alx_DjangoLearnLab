7f4675fa8e8db9d73aead57df30318bdc29824ae
eskainah <eskainah@gmail.com>
2024-10-05 23:04:06 +0300

    edited files

--- ALX_BE_CapstoneProject/Event_Management/event/views.py
+++ ALX_BE_CapstoneProject/Event_Management/event/views.py
@@ -1,94 +1,12 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
-
-
-from django.contrib.auth import login, authenticate, logout, get_user_model
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .models import Event, Profile
-from .serializers import ProfileSerializer, EventSerializer, CustomUserSerializer, AuthTokenSerializer
+from .models import Event
+from .serializers import ProfileSerializer, EventSerializer
 from rest_framework.permissions import IsAuthenticated
 
-class AuthTokenViewSet(viewsets.ModelViewSet):
-
-    def create(self, request):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email,
-        })
-
-User = get_user_model()
-
-class CustomUserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]  
-
-    @action(detail=False, methods=['post'])
-    def register(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({'user_id': user.id, 'username': user.username}, status=201)
-        return Response(serializer.errors, status=400)
-
-    @action(detail=False, methods=['post'])
-    def login(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return Response({'message': 'Login successful'}, status=200)
-        return Response({'error': 'Invalid credentials'}, status=400)
-
-    @action(detail=False, methods=['post'])
-    def logout(self, request):
-        logout(request)
-        return Response({'message': 'Logout successful'}, status=204)
-
-# View for User profile
-class ProfileViewSet(viewsets.ModelViewSet):
-    serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Profile.objects.filter(user=self.request.user)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=self.request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, *args, **kwargs):
-        profile = get_object_or_404(Profile, user=request.user)
-        serializer = self.get_serializer(profile)
-        return Response(serializer.data)
-
-    def update(self, request, *args, **kwargs):
-        profile = get_object_or_404(Profile, user=request.user)
-        serializer = self.get_serializer(profile, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, *args, **kwargs):
-        profile = get_object_or_404(Profile, user=request.user)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
