9df36fd22e1c2e3cbbeb13bf71f43f719a89c260
eskainah <eskainah@gmail.com>
2024-10-06 02:53:31 +0300

    all update without effecting the require fields

--- BE_CapstoneProject/events/serializers.py
+++ BE_CapstoneProject/events/serializers.py
@@ -5,16 +5,22 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['title', 'description', 'date_time', 'location', 'organizer', 'capacity', 'created_date']
-    
+        fields = ['title', 'description', 'date_time', 'location', 'capacity', 'current_capacity', 'created_date']
+        read_only_fields = ['organizer']
+        extra_kwargs = {
+            'title': {'required': True}, 
+            'location': {'required': True},  #Ensure title is require when creating an event
+        }
+        
     def validate(self, attrs):
         if 'date_time' in attrs and attrs['date_time'] < timezone.now():
             raise serializers.ValidationError("Please enter a date in the future")
-        if 'title' not in attrs or not attrs['title']:
+        if 'title' in attrs and not attrs['title']:
             raise serializers.ValidationError("Title is required.")
-        if 'location' not in attrs or not attrs['location']:
+        if 'location' in attrs and not attrs['location']:
             raise serializers.ValidationError("Location is required.")
         if attrs.get('current_capacity', 0) > attrs.get('capacity', 0):
             raise serializers.ValidationError("Current capacity cannot exceed maximum capacity.")
         return attrs
        
+    
