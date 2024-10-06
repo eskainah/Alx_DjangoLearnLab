1ced46d657d21cfbb13df0e5be4ba37a4074f4e5
eskainah <eskainah@gmail.com>
2024-10-06 03:29:03 +0300

    update permission

--- BE_CapstoneProject/events/permissions.py
+++ BE_CapstoneProject/events/permissions.py
@@ -5,9 +5,7 @@ class IsEventOwner(permissions.BasePermission):
     
     # allow owners to update or delete the event
     def has_object_permission(self, request, view, obj):
-       
         return obj.organizer == request.user
     # Allow authenticated users to list or create events
     def has_permission(self, request, view):
-       
         return request.user and request.user.is_authenticated
